9ca8b5230e5668d5681ad3534a8367c3f07b8ac6
Cgaliano <cgaliano@smarthub.cl>
2019-10-01 15:32:33 -0300

    edit tray-list Component.ts

--- src/app/components/tray-list/tray-list.component.ts
+++ src/app/components/tray-list/tray-list.component.ts
@@ -16,41 +16,7 @@ import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-tray-list',
-  template: `
-  <br>
-  <br>
-  <br>
-  <br>
-  <mat-accordion>
-  <mat-expansion-panel>
-      <mat-expansion-panel-header>
-          <mat-panel-title>
-              Busqueda por fecha
-          </mat-panel-title>
-          <mat-panel-description>
-              Selecciona una fecha
-          </mat-panel-description>
-      </mat-expansion-panel-header>
-      <mat-form-field>
-          <input matInput [matDatepicker]="picker" placeholder="" (dateInput)="obtenerFecha('input', $event)">
-          <mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
-          <mat-datepicker #picker></mat-datepicker>
-      </mat-form-field>
-  </mat-expansion-panel>
-  <mat-expansion-panel (opened)="panelOpenState = true" (closed)="panelOpenState = false">
-      <mat-expansion-panel-header>
-          <mat-panel-title>
-              Busqueda por rango de fechas
-          </mat-panel-title>
-          <mat-panel-description>
-              Selecciona un Rango de Fechas
-          </mat-panel-description>
-      </mat-expansion-panel-header>
-      <input id="rangeDate" name="rangeDate" #input="ngModel" [(ngModel)]="!rangeDate" type="text" bsDaterangepicker class="form-control" />
-      <button mat-button color="primary" (click)="obtainDate(input.viewModel)">Buscar</button>
-  </mat-expansion-panel>
-</mat-accordion>
-<router-outlet></router-outlet> `,
+  templateUrl: './tray-list.component.html',
   styleUrls: ['./tray-list.component.css']
 
 })
