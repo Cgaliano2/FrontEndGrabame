623d522284c120e5f7f8a71022b0423bcfd26e33
Cgaliano <cgaliano@smarthub.cl>
2019-10-17 12:52:00 -0300

    intento 5 de deploy!

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+var port = process.env.PORT || 5000;
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.port || 5000);
+app.listen(port);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
